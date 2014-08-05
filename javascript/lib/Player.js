c00d6d11baabd72e62293cf0a4f977d11699a389
Jeremy Fox <foxjerem@gmail.com>
2014-08-05 11:16:21 +0100

    Added autpick method

--- javascript/lib/Player.js
+++ javascript/lib/Player.js
@@ -2,6 +2,13 @@ function Player(name) {
   this.name = name;
 };
 
+Player.prototype.CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+
 Player.prototype.picks = function(pick) {
   this.pick = pick;
+};
+
+Player.prototype.autopick = function() {
+	this.picks(this.CHOICES[Math.floor(Math.random()*4)]);
 };
